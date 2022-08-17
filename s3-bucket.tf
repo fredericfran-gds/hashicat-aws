f88a1656fca3c6b5eb063cab204ee5a89e393459
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2022-08-17 12:25:21 +0000

    bucket prefix conflicts with bucket

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,6 @@
 module "s3_bucket" {
   source = "app.terraform.io/workshop-ff/s3-bucket/aws"
   version = "2.8.0"
-  bucket_prefix = "ff"
   bucket = "ff-test"
   acl    = "private"
   versioning = {
