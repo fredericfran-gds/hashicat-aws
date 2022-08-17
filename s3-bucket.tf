c5922c32bf67ab07873b2a0d273d734c1ba0ad03
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2022-08-17 12:14:16 +0000

    source

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,7 @@
 module "s3_bucket" {
   source = "app.terraform.io/workshop-ff/s3-bucket/aws"
   version = "2.8.0"
-
-  bucket = "gaurav"
   acl    = "private"
-  bucket_prefix = "ff"
-
   versioning = {
     enabled = true
   }
