857dc6c4a25e7c6bc1e65e4c6b20614598ef5c74
Frederic Francois <frederic.francois@digital.cabinet-office.gov.uk>
2022-08-17 12:21:30 +0000

    test bucket prefix

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,7 @@
 module "s3_bucket" {
   source = "app.terraform.io/workshop-ff/s3-bucket/aws"
   version = "2.8.0"
+  bucket_prefix = "ff"
   bucket = "ff-test"
   acl    = "private"
   versioning = {
